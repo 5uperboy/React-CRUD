80dc5cf74ab23d7df8e84de202a5a556a401eedf
Johnrod <johnrodjardeleza09@gmail.com>
2024-02-12 12:15:12 +0800

    made content dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,31 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
 // eslint-disable-next-line react/prop-types
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button 
-      color={showAdd ? "red" : "green"}
-      text={showAdd ? "Close" : "Add"} 
-      onClick={onAdd} />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
-  )
-}
+  );
+};
 // Default props
 Header.defaultProps = {
   title: "Task Tracker",
-}
+};
 // PropTypes required
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-}
+};
 
-export default Header
+export default Header;
